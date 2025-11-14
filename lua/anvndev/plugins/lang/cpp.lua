c5b7360ca90dc9bc19d7cd1df2bb61f13d432262
andev0x <anvndev@gmail.com>
2025-11-14 12:37:25 +0700

    refactor(cpp): optimize configuration

--- lua/anvndev/plugins/lang/cpp.lua
+++ lua/anvndev/plugins/lang/cpp.lua
@@ -1,124 +1,159 @@
 -- ~/.config/nvim/lua/anvndev/plugins/lang/cpp.lua
--- C/C++ language configuration
+-- C/C++ language configuration for Neovim
 
 return {
-  {
-    "p00f/clangd_extensions.nvim",
-    ft = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-    
-    config = function()
-      require("clangd_extensions").setup({
-        server = {
-          
-          -- i.e. the arguments to vim.lsp.config("clangd", {})
-          cmd = {
-            "clangd",
-            "--background-index",
-            "--pch-storage=memory",
-            "--clang-tidy",
-            "--suggest-missing-includes",
-            "--all-scopes-completion",
-            "--completion-style=detailed",
-            "--header-insertion=iwyu",
-            "--header-insertion-decorators",
-            "--enable-config",
-            "--offset-encoding=utf-16",
-          },
-          capabilities = {
-            offsetEncoding = { "utf-16" },
-          },
-          on_attach = function(client, bufnr)
-            -- Enable inlay hints
-            require("clangd_extensions.inlay_hints").setup_autocmd()
-            require("clangd_extensions.inlay_hints").set_inlay_hints()
-          end,
-        },
-        extensions = {
-          -- defaults:
-          -- Automatically set inlay hints (type hints)
-          autoSetHints = true,
-          -- These apply to the default ClangdSetInlayHints command
-          inlay_hints = {
-            inline = vim.fn.has("nvim-0.10") == 1,
-            -- Only show inlay hints for the current line
-            only_current_line = false,
-            -- Event which triggers a refersh of the inlay hints.
-            -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
-            -- not that this may cause  higher CPU usage.
-            -- This option is only respected when only_current_line and
-            -- autoSetHints both are true.
-            only_current_line_autocmd = "CursorHold",
-            -- whether to show parameter hints with the inlay hints or not
-            show_parameter_hints = true,
-            -- prefix for parameter hints
-            parameter_hints_prefix = "<- ",
-            -- prefix for all the other hints (type, chaining)
-            other_hints_prefix = "=> ",
-            -- whether to align to the length of the longest line in the file
-            max_len_align = false,
-            -- padding from the left if max_len_align is true
-            max_len_align_padding = 1,
-            -- whether to align to the extreme right or not
-            right_align = false,
-            -- padding from the right if right_align is true
-            right_align_padding = 7,
-            -- The color of the hints
-            highlight = "Comment",
-            -- The highlight group priority for extmark
-            priority = 100,
-          },
-          ast = {
-            -- These are unicode, should work without any setup
-            role_icons = {
-              type = "🄣",
-              declaration = "🄓",
-              expression = "🄔",
-              statement = ";",
-              specifier = "🄢",
-              ["template argument"] = "🆃",
-            },
-            kind_icons = {
-              Compound = "🄲",
-              Recovery = "🅁",
-              TranslationUnit = "🅄",
-              PackExpansion = "🄿",
-              TemplateTypeParm = "🅃",
-              TemplateTemplateParm = "🅃",
-              TemplateParamObject = "🅃",
-            },
-            highlights = {
-              detail = "Comment",
-            },
-          },
-          memory_usage = {
-            border = "none",
-          },
-          symbol_info = {
-            border = "none",
-          },
-        },
-      })
-    end,
-  },
-  
-  -- C/C++ test integration
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "antoinemadec/FixCursorHold.nvim",
-      "alfaix/neotest-gtest",
-    },
-    config = function()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-gtest")({
-            gtest_command = "gtest",
-            gtest_args = { "--gtest_color=yes" },
-          }),
-        },
-      })
-    end,
-  },
+	---------------------------------------------------------------------------
+	-- 🛠️ Clangd (LSP) Extensions
+	---------------------------------------------------------------------------
+	{
+		"p00f/clangd_extensions.nvim",
+		ft = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+
+		config = function()
+			----------------------------------------------------------------------------
+			-- Diagnostics configuration (global)
+			----------------------------------------------------------------------------
+			vim.diagnostic.config({
+				virtual_text = false, -- disable inline diagnostics
+				signs = true, -- show only signs in gutter
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
+
+			----------------------------------------------------------------------------
+			-- Beautiful diagnostic icons (replace E/W/H)
+			----------------------------------------------------------------------------
+			local signs = {
+				Error = " ", -- nf-fa-times_circle
+				Warn = " ", -- nf-fa-warning
+				Hint = " ", -- nf-oct-light_bulb
+				Info = "󰙎 ", -- nf-fa-info_circle
+			}
+
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
+
+			----------------------------------------------------------------------------
+			-- Clangd Extensions setup
+			----------------------------------------------------------------------------
+			require("clangd_extensions").setup({
+				server = {
+					cmd = {
+						"clangd",
+						"--background-index",
+						"--pch-storage=memory",
+						"--clang-tidy",
+						"--suggest-missing-includes",
+						"--all-scopes-completion",
+						"--completion-style=detailed",
+						"--header-insertion=iwyu",
+						"--header-insertion-decorators",
+						"--enable-config",
+						"--offset-encoding=utf-16",
+					},
+
+					-- Required for Neovim 0.10+
+					capabilities = {
+						offsetEncoding = { "utf-16" },
+					},
+
+					on_attach = function(client, bufnr)
+						local map = vim.keymap.set
+						local opts = { noremap = true, silent = true, buffer = bufnr }
+
+						----------------------------------------------------------------------
+						-- LSP Mappings
+						----------------------------------------------------------------------
+						map("n", "K", vim.lsp.buf.hover, opts)
+						map("n", "gd", vim.lsp.buf.definition, opts)
+						map("n", "gr", vim.lsp.buf.references, opts)
+						map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+						map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+						map("n", "<leader>f", function()
+							vim.lsp.buf.format({ async = true })
+						end, opts)
+
+						----------------------------------------------------------------------
+						-- Enable Clangd inlay hints
+						----------------------------------------------------------------------
+						require("clangd_extensions.inlay_hints").setup_autocmd()
+						require("clangd_extensions.inlay_hints").set_inlay_hints()
+					end,
+				},
+
+				------------------------------------------------------------------------
+				-- Extensions Settings
+				------------------------------------------------------------------------
+				extensions = {
+					-- Auto set inlay hints
+					autoSetHints = true,
+
+					inlay_hints = {
+						inline = vim.fn.has("nvim-0.10") == 1,
+						only_current_line = false,
+						only_current_line_autocmd = "CursorHold",
+						show_parameter_hints = true,
+						parameter_hints_prefix = "<- ",
+						other_hints_prefix = "=> ",
+						max_len_align = false,
+						right_align = false,
+						highlight = "Comment",
+						priority = 100,
+					},
+
+					-- AST visualizer icons
+					ast = {
+						role_icons = {
+							type = "🄣",
+							declaration = "🄓",
+							expression = "🄔",
+							statement = ";",
+							specifier = "🄢",
+							["template argument"] = "🆃",
+						},
+						kind_icons = {
+							Compound = "🄲",
+							Recovery = "🅁",
+							TranslationUnit = "🅄",
+							PackExpansion = "🄿",
+							TemplateTypeParm = "🅃",
+							TemplateTemplateParm = "🅃",
+							TemplateParamObject = "🅃",
+						},
+						highlights = { detail = "Comment" },
+					},
+
+					memory_usage = { border = "none" },
+					symbol_info = { border = "none" },
+				},
+			})
+		end,
+	},
+
+	---------------------------------------------------------------------------
+	-- 🧪 C/C++ Unit Testing (GoogleTest)
+	---------------------------------------------------------------------------
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"antoinemadec/FixCursorHold.nvim",
+			"alfaix/neotest-gtest",
+		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-gtest")({
+						gtest_command = "gtest",
+						gtest_args = { "--gtest_color=yes" },
+					}),
+				},
+			})
+		end,
+	},
 }
+
